8760c4f90522bc8a5bf0922949f21e5a486f5e3c
Codie Morgan <codiemorgan@gmail.com>
2017-09-01 21:54:52 +0200

    Preperation to implement NodeInput/Output support within Node itself.

--- Nodes.Core Plugin/Nodes.Core/Node.cs
+++ Nodes.Core Plugin/Nodes.Core/Node.cs
@@ -8,22 +8,19 @@ namespace UNEB
 {
     /// <summary>
     /// Baseclass for all Node Types. Inheriting Types must implement the <see cref="System.SerializableAttribute"/>.
+    /// TODO: NodeInput/NodeOuput implementation here.
     /// </summary>
     [Serializable]
     public abstract class Node : GraphObject
     {
 
-        /// <summary>
-        /// All connection references from this node.
-        /// </summary>
         [SerializeField]
-        List<Reference> m_Connections = new List<Reference>();
+        List<NodeInputBase<Node>>  m_Inputs = new List<NodeInputBase<Node>>();
+
+        [SerializeField]
+        List<NodeOutputBase<Node>> m_Outputs = new List<NodeOutputBase<Node>>();
+
  
-        /// <summary>
-        /// Cached array of all known types which inherit from <see cref="Node"/>, not including Abstract types.
-        /// This includes types in all loaded .NET/Mono assemblies, including those defined by scripts in the unity assets folder.
-        /// </summary>
-        static Type[] m_AllKnownInheritingNodeTypes;
 
 #region Events
 
@@ -36,16 +33,13 @@ namespace UNEB
         #endregion
 
 
- #region Properties
-         
- 
+#region Properties
 
-
-
-        //public Connection GetConnection(int index)
-        //{
-        //    return m_Connections[index].TryGetValueAsType<Connection>();
-        //}
+        /// <summary>
+        /// Cached array of all known types which inherit from <see cref="Node"/>, not including Abstract types.
+        /// This includes types in all loaded .NET/Mono assemblies, including those defined by scripts in the unity assets folder.
+        /// </summary>
+        static Type[] m_AllKnownInheritingNodeTypes;
 
         /// <summary>
         /// Returns an array of all known types which inherit from <see cref="Node"/>, not including abstract types.
